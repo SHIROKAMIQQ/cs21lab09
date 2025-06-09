9c07511e812844cdfd25b7ca3fff80c667529ce6
SHIROKAMIQQ <migzguiang@gmail.com>
2025-06-09 16:53:10 -0700

    feat(task1): basic implementation

---
+++ task1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    double approx = 0;
+    double n = 100000000;
+
+    for (int i = 0; i <= n; i++) {
+        double numerator = 1;
+        if (i%2 == 1) {
+            numerator *= -1;
+        }
+        double denominator = 2*i + 1;
+
+        double term = numerator / denominator;
+        approx += term;
+    }
+    approx *= 4;
+    printf("%.10lf\n", approx);
+}
